8b6ef2c69bf32043a61795e329a2834b27e486fe
im-hyemm <57058302+im-hyemm@users.noreply.github.com>
2020-02-17 22:20:32 +0900

    Update bydate.js

--- js/bydate.js
+++ js/bydate.js
@@ -1,333 +1,333 @@
 var chart_d0d7fb0e988d45cc871cd479ef340c28 = echarts.init(
-    document.getElementById('d0d7fb0e988d45cc871cd479ef340c28'), 'white', {renderer: 'canvas'});
-var option_d0d7fb0e988d45cc871cd479ef340c28 = {
-"animation": true,
-"animationThreshold": 2000,
-"animationDuration": 1000,
-"animationEasing": "cubicOut",
-"animationDelay": 0,
-"animationDurationUpdate": 300,
-"animationEasingUpdate": "cubicOut",
-"animationDelayUpdate": 0,
-"color": [
-"maroon",
-"firebrick",
-"orangered",
-"gray"
-],
-"series": [
-{
-    "type": "bar",
-    "name": "1\ucc28",
-    "data": [
-        1.0,
-        0.0,
-        0.0,
-        1.0,
-        0.0,
-        0.0,
-        1.0,
-        1.0,
-        0.0,
-        0.0,
-        0.0,
-        2.0,
-        1.0,
-        0.0,
-        2.0,
-        1.0,
-        0.0,
-        1.0,
-        2.0,
-        0.0,
-        0.0,
-        2.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0
+            document.getElementById('d0d7fb0e988d45cc871cd479ef340c28'), 'white', {renderer: 'canvas'});
+        var option_d0d7fb0e988d45cc871cd479ef340c28 = {
+    "animation": true,
+    "animationThreshold": 2000,
+    "animationDuration": 1000,
+    "animationEasing": "cubicOut",
+    "animationDelay": 0,
+    "animationDurationUpdate": 300,
+    "animationEasingUpdate": "cubicOut",
+    "animationDelayUpdate": 0,
+    "color": [
+        "maroon",
+        "firebrick",
+        "orangered",
+        "gray"
     ],
-    "stack": "stack1",
-    "barCategoryGap": "20%",
-    "label": {
-        "show": false,
-        "position": "top",
-        "margin": 8
-    },
-    "rippleEffect": {
-        "show": true,
-        "brushType": "stroke",
-        "scale": 2.5,
-        "period": 4
-    }
-},
-{
-    "type": "bar",
-    "name": "2\ucc28",
-    "data": [
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        1.0,
-        1.0,
-        1.0,
-        0.0,
-        0.0,
-        0.0,
-        2.0,
-        2.0,
-        0.0,
-        0.0,
-        1.0,
-        1.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0
-    ],
-    "stack": "stack1",
-    "barCategoryGap": "20%",
-    "label": {
-        "show": false,
-        "position": "top",
-        "margin": 8
-    },
-    "rippleEffect": {
-        "show": true,
-        "brushType": "stroke",
-        "scale": 2.5,
-        "period": 4
-    }
-},
-{
-    "type": "bar",
-    "name": "3\ucc28",
-    "data": [
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        2.0,
-        0.0,
-        1.0,
-        0.0,
-        0.0,
-        0.0,
-        1.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0
-    ],
-    "stack": "stack1",
-    "barCategoryGap": "20%",
-    "label": {
-        "show": false,
-        "position": "top",
-        "margin": 8
-    },
-    "rippleEffect": {
-        "show": true,
-        "brushType": "stroke",
-        "scale": 2.5,
-        "period": 4
-    }
-},
-{
-    "type": "bar",
-    "name": "\ud655\uc778 \uc911",
-    "data": [
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        1.0,
-        1.0
-    ],
-    "stack": "stack1",
-    "barCategoryGap": "20%",
-    "label": {
-        "show": false,
-        "position": "top",
-        "margin": 8
-    },
-    "rippleEffect": {
-        "show": true,
-        "brushType": "stroke",
-        "scale": 2.5,
-        "period": 4
-    }
-}
-],
-"legend": [
-{
-    "data": [
-        "1\ucc28",
-        "2\ucc28",
-        "3\ucc28",
-        "\ud655\uc778 \uc911"
-    ],
-    "selected": {
-        "1\ucc28": true,
-        "2\ucc28": true,
-        "3\ucc28": true,
-        "\ud655\uc778 \uc911": true
-    },
-    "show": true,
-    "padding": 5,
-    "itemGap": 10,
-    "itemWidth": 25,
-    "itemHeight": 14
-}
-],
-"tooltip": {
-"show": true,
-"trigger": "item",
-"triggerOn": "mousemove|click",
-"axisPointer": {
-    "type": "line"
-},
-"textStyle": {
-    "fontSize": 14
-},
-"borderWidth": 0
-},
-"xAxis": [
-{
-    "show": true,
-    "scale": false,
-    "nameLocation": "end",
-    "nameGap": 15,
-    "gridIndex": 0,
-    "inverse": false,
-    "offset": 0,
-    "splitNumber": 5,
-    "minInterval": 0,
-    "splitLine": {
-        "show": false,
-        "lineStyle": {
-            "width": 1,
-            "opacity": 1,
-            "curveness": 0,
-            "type": "solid"
+    "series": [
+        {
+            "type": "bar",
+            "name": "1\ucc28",
+            "data": [
+                1.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                1.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                2.0,
+                1.0,
+                0.0,
+                2.0,
+                1.0,
+                0.0,
+                1.0,
+                2.0,
+                0.0,
+                0.0,
+                2.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0
+            ],
+            "stack": "stack1",
+            "barCategoryGap": "20%",
+            "label": {
+                "show": false,
+                "position": "top",
+                "margin": 8
+            },
+            "rippleEffect": {
+                "show": true,
+                "brushType": "stroke",
+                "scale": 2.5,
+                "period": 4
+            }
+        },
+        {
+            "type": "bar",
+            "name": "2\ucc28",
+            "data": [
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                1.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                2.0,
+                2.0,
+                0.0,
+                0.0,
+                1.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0
+            ],
+            "stack": "stack1",
+            "barCategoryGap": "20%",
+            "label": {
+                "show": false,
+                "position": "top",
+                "margin": 8
+            },
+            "rippleEffect": {
+                "show": true,
+                "brushType": "stroke",
+                "scale": 2.5,
+                "period": 4
+            }
+        },
+        {
+            "type": "bar",
+            "name": "3\ucc28",
+            "data": [
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                2.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0
+            ],
+            "stack": "stack1",
+            "barCategoryGap": "20%",
+            "label": {
+                "show": false,
+                "position": "top",
+                "margin": 8
+            },
+            "rippleEffect": {
+                "show": true,
+                "brushType": "stroke",
+                "scale": 2.5,
+                "period": 4
+            }
+        },
+        {
+            "type": "bar",
+            "name": "\ud655\uc778 \uc911",
+            "data": [
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                1.0
+            ],
+            "stack": "stack1",
+            "barCategoryGap": "20%",
+            "label": {
+                "show": false,
+                "position": "top",
+                "margin": 8
+            },
+            "rippleEffect": {
+                "show": true,
+                "brushType": "stroke",
+                "scale": 2.5,
+                "period": 4
+            }
         }
+    ],
+    "legend": [
+        {
+            "data": [
+                "1\ucc28",
+                "2\ucc28",
+                "3\ucc28",
+                "\ud655\uc778 \uc911"
+            ],
+            "selected": {
+                "1\ucc28": true,
+                "2\ucc28": true,
+                "3\ucc28": true,
+                "\ud655\uc778 \uc911": true
+            },
+            "show": true,
+            "padding": 5,
+            "itemGap": 10,
+            "itemWidth": 25,
+            "itemHeight": 14
+        }
+    ],
+    "tooltip": {
+        "show": true,
+        "trigger": "item",
+        "triggerOn": "mousemove|click",
+        "axisPointer": {
+            "type": "line"
+        },
+        "textStyle": {
+            "fontSize": 14
+        },
+        "borderWidth": 0
     },
-    "data": [
-        "01/19",
-        "01/20",
-        "01/21",
-        "01/22",
-        "01/23",
-        "01/24",
-        "01/25",
-        "01/26",
-        "01/27",
-        "01/28",
-        "01/29",
-        "01/30",
-        "01/31",
-        "02/01",
-        "02/02",
-        "02/03",
-        "02/04",
-        "02/05",
-        "02/06",
-        "02/07",
-        "02/08",
-        "02/09",
-        "02/10",
-        "02/11",
-        "02/12",
-        "02/13",
-        "02/14",
-        "02/15",
-        "02/16",
-        "02/17"
+    "xAxis": [
+        {
+            "show": true,
+            "scale": false,
+            "nameLocation": "end",
+            "nameGap": 15,
+            "gridIndex": 0,
+            "inverse": false,
+            "offset": 0,
+            "splitNumber": 5,
+            "minInterval": 0,
+            "splitLine": {
+                "show": false,
+                "lineStyle": {
+                    "width": 1,
+                    "opacity": 1,
+                    "curveness": 0,
+                    "type": "solid"
+                }
+            },
+            "data": [
+                "01/19",
+                "01/20",
+                "01/21",
+                "01/22",
+                "01/23",
+                "01/24",
+                "01/25",
+                "01/26",
+                "01/27",
+                "01/28",
+                "01/29",
+                "01/30",
+                "01/31",
+                "02/01",
+                "02/02",
+                "02/03",
+                "02/04",
+                "02/05",
+                "02/06",
+                "02/07",
+                "02/08",
+                "02/09",
+                "02/10",
+                "02/11",
+                "02/12",
+                "02/13",
+                "02/14",
+                "02/15",
+                "02/16",
+                "02/17"
+            ]
+        }
+    ],
+    "yAxis": [
+        {
+            "show": true,
+            "scale": false,
+            "nameLocation": "end",
+            "nameGap": 15,
+            "gridIndex": 0,
+            "inverse": false,
+            "offset": 0,
+            "splitNumber": 5,
+            "minInterval": 0,
+            "splitLine": {
+                "show": false,
+                "lineStyle": {
+                    "width": 1,
+                    "opacity": 1,
+                    "curveness": 0,
+                    "type": "solid"
+                }
+            }
+        }
+    ],
+    "title": [
+        {
+            "text": "\ub0a0\uc9dc\ubcc4 \ud655\uc9c4\uc790 \uc218(\uba85)",
+            "padding": 5,
+            "itemGap": 10
+        }
     ]
-}
-],
-"yAxis": [
-{
-    "show": true,
-    "scale": false,
-    "nameLocation": "end",
-    "nameGap": 15,
-    "gridIndex": 0,
-    "inverse": false,
-    "offset": 0,
-    "splitNumber": 5,
-    "minInterval": 0,
-    "splitLine": {
-        "show": false,
-        "lineStyle": {
-            "width": 1,
-            "opacity": 1,
-            "curveness": 0,
-            "type": "solid"
-        }
-    }
-}
-],
-"title": [
-{
-    "text": "\ub0a0\uc9dc\ubcc4 \ud655\uc9c4\uc790 \uc218(\uba85)",
-    "padding": 5,
-    "itemGap": 10
-}
-]
 };
-chart_d0d7fb0e988d45cc871cd479ef340c28.setOption(option_d0d7fb0e988d45cc871cd479ef340c28);
+        chart_d0d7fb0e988d45cc871cd479ef340c28.setOption(option_d0d7fb0e988d45cc871cd479ef340c28);

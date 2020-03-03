fdc155b307c9a59a78e9e81ea2bbc424d8297f6b
popo97kr <45965766+popo97kr@users.noreply.github.com>
2020-03-04 00:44:32 +0900

    no message

--- app.js
+++ app.js
@@ -39,4 +39,64 @@
         preObject4.innerText=JSON.stringify(snap.val(),null,3);
     });
 
+
+
+    const preObject5 = document.getElementById('사망자');
+    const dbRefObject5 = firebase.database().ref().child('지역별확진자수');
+
+    dbRefObject5.on('value',snap=>{
+        preObject5.innerText=JSON.stringify(snap.val(),null,3);
+        var json = JSON.parse(preObject5.innerText)
+        for (key in json){
+            console.log('key:' + key + ' / ' + 'value:' + json[key])
+        }
+        google.charts.load('current', {'packages':['bar','corechart']});
+            google.charts.setOnLoadCallback(schedulerSuccessAndFailChart);
+
+                function schedulerSuccessAndFailChart() {
+                    var data = google.visualization.arrayToDataTable([
+                          ["Title","전체", {role:'annotation'}],
+                          ['대구', json['대구'], json['대구']],
+                            ['경북',json['경북'], json['경북']],
+                            ['서울', json['서울'], json['서울']],
+                            ['경기', json['경기'], json['경기']],
+                            ['부산', json['부산'], json['부산']],
+                            ['충남', json['충남'], json['충남']],
+                            ['경남', json['경남'], json['경남']],
+                            ['울산', json['울산'], json['울산']],
+                            ['강원', json['강원'], json['강원']],
+                            ['대전', json['대전'], json['대전']],
+                            ['충북', json['충북'], json['충북']],
+                            ['광주', json['광주'], json['광주']],
+                            ['인천', json['인천'], json['인천']],
+                            ['전북', json['전북'], json['전북']],
+                            ['전남', json['전남'], json['전남']],
+                            ['세종', json['세종'], json['세종']]    
+                  ]);
+            
+                  var barChartOption = {
+                          bars: 'vertical',
+                          height :400,
+                          vAxis:{textStyle:{fontSize:13}},
+                          hAxis:{textStyle:{fontSize:13}},
+                          legend: { position: "none", textStyle: {fontSize: 11} }, //'top'
+                          colors: ['#ca3433'],// '#5e64c1','#d3e9ff'
+                          tooltip:{textStyle : {fontSize:12}, showColorCode : true},
+                          annotations: {
+                              textStyle: {
+                                fontSize: 11,
+                                color: '#871b47'
+                              }
+                          },
+                          bar: { groupWidth: "85%" },
+                          chartArea:{width:'85%',top:'30',bottom:'0'}
+                    };
+            
+                  var chart = new google.visualization.BarChart(document.getElementById('region'));
+            
+                  chart.draw(data, barChartOption);
+                  
+                }
+    });
+
 }());

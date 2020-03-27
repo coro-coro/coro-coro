5ae3b0edf522e2e97ffa3963c5a1a90e9bda9552
Jihye Kwon <45965766+popo97kr@users.noreply.github.com>
2020-03-27 11:26:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -90,8 +90,8 @@
             }
             var s = items[i][0].concat('증감');
             console.log(s);
-            console.log((items[i][1].toString().concat('(','+',json[s].toString(),')')));
-            lst.push([items[i][0],items[i][1],(items[i][1].toString().concat('(','+',json[s].toString(),')'))]);
+            console.log((items[i][1].toString().concat(json[s].toString())));
+            lst.push([items[i][0],items[i][1],(items[i][1].toString().concat(json[s].toString()))]);
         }
         google.charts.load('current', {'packages':['bar','corechart']});
             google.charts.setOnLoadCallback(schedulerSuccessAndFailChart);
